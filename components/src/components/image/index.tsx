12542d84f65b95e648268574a592d765d2edc80f
Tiago Peres França <tiago.franca@zup.com.br>
2022-01-12 17:32:21 -0300

    remove any

--- components/src/components/image/index.tsx
+++ components/src/components/image/index.tsx
@@ -2,9 +2,8 @@ import { React } from '@zup-it/beagle-backend-core'
 import { StyledDefaultComponent } from '../../style/styled'
 import { ImageFC, AllProps } from './types'
 
-const ImageComponent = ({ id, style, type, ...props }: AllProps<'local' | 'remote'>) => {
-  (props as any)['_beagleImagePath_'] = type
-  return <StyledDefaultComponent name="image" id={id} style={style} properties={props} />
-}
+const ImageComponent = ({ id, style, type, ...props }: AllProps<'local' | 'remote'>) => (
+  <StyledDefaultComponent name="image" id={id} style={style} properties={{ ...props, '_beagleImagePath_': type }} />
+)
 
 export const Image = ImageComponent as ImageFC
