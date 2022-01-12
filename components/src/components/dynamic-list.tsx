50fa24ba06a193aeccf70bb9914ad5f3be8d2d91
Tiago Peres França <tiago.franca@zup.com.br>
2022-01-12 15:50:05 -0300

    screen, listView, gridView

---
+++ components/src/components/dynamic-list.tsx
@@ -0,0 +1,32 @@
+import { React, Actions, FC, Expression, Component } from '@zup-it/beagle-backend-core'
+import { DefaultComponent } from '../default-component'
+
+interface TemplateManagerItem {
+  case: string,
+  view: Component,
+}
+
+interface ListViewProps {
+  direction?: 'VERTICAL' | 'HORIZONTAL',
+  onInit?: Actions,
+  dataSource: Expression<any[]>,
+  templates: TemplateManagerItem[],
+  isScrollIndicatorVisible?: boolean,
+  onScrollEnd?: Actions,
+  scrollEndThreshold?: number,
+  iteratorName?: string,
+  key?: string,
+  children: never,
+}
+
+interface GridViewProps extends ListViewProps {
+  spanCount?: number,
+}
+
+export const ListView: FC<ListViewProps> = ({ id, context, ...props }) => (
+  <DefaultComponent name="listView" id={id} context={context} properties={props} />
+)
+
+export const GridView: FC<GridViewProps> = ({ id, context, ...props }) => (
+  <DefaultComponent name="gridView" id={id} context={context} properties={props} />
+)
