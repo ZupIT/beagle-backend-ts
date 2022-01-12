36eddcd70b8dc00fe1e03341117b5bd8cbfedf65
Arthur Bleil <arthur.bleil@zup.com.br>
2022-01-12 15:51:25 -0300

    create pullToRefresh component
    Signed-off-by: Arthur Bleil <arthur.bleil@zup.com.br>

--- components/src/components/index.ts
+++ components/src/components/index.ts
@@ -1,5 +1,6 @@
 export { Button } from './button'
 export { Container } from './container'
 export { Image } from './image'
+export { PullToRefresh } from './pull-to-refresh'
 export { TextInput } from './text-input'
 export { Text } from './text'
