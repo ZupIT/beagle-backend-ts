9f1e4d9745b00f65c45af48857d45bf6c7fc49c8
Arthur Bleil <arthur.bleil@zup.com.br>
2022-01-12 16:02:46 -0300

    create webview component
    Signed-off-by: Arthur Bleil <arthur.bleil@zup.com.br>

---
+++ components/src/components/webview.tsx
@@ -0,0 +1,13 @@
+import { React, FC, Expression } from '@zup-it/beagle-backend-core'
+import { StyledDefaultComponent, Style } from '../style/styled'
+import { Accessibility, Theme } from '../types'
+
+interface SpecificWebviewProps {
+  url: string | Expression<string>,
+}
+
+type WebviewProps = SpecificWebviewProps & Accessibility & Theme & Style
+
+export const Webview: FC<WebviewProps> = ({ id, style, ...props }) => (
+  <StyledDefaultComponent name="webview" id={id} style={style} properties={props} />
+)
