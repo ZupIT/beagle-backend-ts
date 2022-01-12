998b195846483530e57126eda4f614bfd70b0d77
Tiago Peres França <tiago.franca@zup.com.br>
2022-01-12 18:24:35 -0300

    simplifies navigation actions

--- core/src/actions/navigation.ts
+++ core/src/actions/navigation.ts
@@ -1,5 +1,7 @@
-import { HttpMethod } from '..'
+import { ContextNode, Expression, HttpMethod } from '..'
+import { ActionInterface } from '../model/action'
 import { ComponentInterface } from '../model/component'
+import { Operation } from '../model/operation'
 import { createCoreAction } from './core-action'
 
 // App navigation
@@ -42,6 +44,7 @@ interface RemoteView {
 export type Route = LocalView | RemoteView
 
 interface BaseNavigationParams {
+  // fixme: this should include both the path and the value
   navigationContext?: any,
 }
 
@@ -53,9 +56,56 @@ interface StackNavigationParams extends RouteNavigationParams {
   controllerId?: string,
 }
 
-export const pushStack = createCoreAction<StackNavigationParams>('pushStack')
-export const pushView = createCoreAction<RouteNavigationParams>('pushView')
-export const popView = createCoreAction<BaseNavigationParams>('popView')
-export const popToView = createCoreAction<RouteNavigationParams<string>>('popToView')
-export const resetStack = createCoreAction<StackNavigationParams>('resetStack')
-export const resetApplication = createCoreAction<StackNavigationParams>('resetApplication')
+const navigator = {
+  pushStack: createCoreAction<StackNavigationParams>('pushStack'),
+  pushView: createCoreAction<RouteNavigationParams>('pushView'),
+  popView: createCoreAction<BaseNavigationParams>('popView'),
+  popToView: createCoreAction<RouteNavigationParams<string>>('popToView'),
+  resetStack: createCoreAction<StackNavigationParams>('resetStack'),
+  resetApplication: createCoreAction<StackNavigationParams>('resetApplication'),
+}
+
+interface PushViewFunction {
+  (url: Expression<string>): ActionInterface,
+  (...args: Parameters<typeof navigator.pushView>): ReturnType<typeof navigator.pushView>,
+}
+
+interface PushStackFunction {
+  (url: Expression<string>): ActionInterface,
+  (...args: Parameters<typeof navigator.pushStack>): ReturnType<typeof navigator.pushStack>,
+}
+
+interface PopToViewFunction {
+  (url: Expression<string>): ActionInterface,
+  (...args: Parameters<typeof navigator.popToView>): ReturnType<typeof navigator.popToView>,
+}
+
+interface ResetStackFunction {
+  (url: Expression<string>): ActionInterface,
+  (...args: Parameters<typeof navigator.resetStack>): ReturnType<typeof navigator.resetStack>,
+}
+
+interface PopViewFunction {
+  (): ActionInterface,
+  (...args: Parameters<typeof navigator.popView>): ReturnType<typeof navigator.popView>,
+}
+
+interface ResetApplicationFunction {
+  (url: Expression<string>): ActionInterface,
+  (...args: Parameters<typeof navigator.resetApplication>): ReturnType<typeof navigator.resetApplication>,
+}
+
+function getParams(options: any, isPopToView = false) {
+  return (typeof options === 'string' || options instanceof ContextNode || options instanceof Operation)
+    ? { route: isPopToView ? options : { url: options } }
+    : options
+}
+
+export const pushView: PushViewFunction = (options: any) => navigator.pushView(getParams(options))
+export const pushStack: PushStackFunction = (options: any) => navigator.pushStack(getParams(options))
+export const resetStack: ResetStackFunction = (options: any) => navigator.resetStack(getParams(options))
+export const resetApplication: ResetApplicationFunction = (options: any) => (
+  navigator.resetApplication(getParams(options))
+)
+export const popToView: PopToViewFunction = (options: any) => navigator.popToView(getParams(options, true))
+export const popView: PopViewFunction = (...args: any[]) => navigator.popView(args[0] ?? {})
--- sample/src/screens/welcome.tsx
+++ sample/src/screens/welcome.tsx
@@ -1,5 +1,6 @@
 import { React, FC } from '@zup-it/beagle-backend-core'
-import { colors, Container, Image } from '@zup-it/beagle-backend-components'
+import { pushView } from '@zup-it/beagle-backend-core/actions/index'
+import { Button, colors, Container, Image, Text } from '@zup-it/beagle-backend-components'
 import { app } from '../app'
 
 export const Welcome: FC = () => (
@@ -13,6 +14,9 @@ export const Welcome: FC = () => (
     }}
   >
     <Image type="remote" path={{ url: 'https://i.ibb.co/rvRN9kv/logo.png' }} style={{ width: 242, height: 225 }} />
+    <Text text="Welcome to the Beagle Playground!" style={{ marginTop: 40 }} />
+    <Text text="Use the panel on the left to start coding!" style={{ marginTop: 5 }} />
+    <Button text="Access the fast guide" style={{ marginTop: 40 }} onPress={pushView('/docs.json')} />
   </Container>
 )
 
