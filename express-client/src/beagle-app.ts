7943efedc56f1d713a3721b5760516cdd334bb83
Tiago Peres França <tiago.franca@zup.com.br>
2022-01-13 13:49:44 -0300

    new nav

--- express-client/src/beagle-app.ts
+++ express-client/src/beagle-app.ts
@@ -1,37 +1,27 @@
 import { forEach } from 'lodash'
 import { Express } from 'express'
-import { HttpMethod, serialize, FC, ContextNode, AnyContextNode } from '@zup-it/beagle-backend-core'
-import { Navigator } from './navigator'
+import { serialize, FC, ContextNode, AnyContextNode } from '@zup-it/beagle-backend-core'
+import { RouteConfig, RouteMap } from './types'
 
 interface Options {
   responseHeaders?: Record<string, string>,
   basePath?: string,
 }
 
-interface ScreenProperties {
-  method?: HttpMethod,
-  path: string,
-  screen: FC<any>,
-}
-
-export type RouteMap = Record<string, FC<any> | Required<Omit<ScreenProperties, 'path'>>>
-
 export class BeagleApp<GlobalContextType = any> {
-  constructor(private express: Express, options: Options = {}) {
+  constructor(private express: Express, routes: RouteMap, options: Options = {}) {
     this.responseHeaders = options.responseHeaders ?? {}
     this.basePath = options.basePath ?? ''
-    this.navigator = new Navigator(this.basePath)
+    this.addRouteMap(routes)
   }
 
   private responseHeaders: Record<string, any>
   private basePath: string
   // The casting below shouldn't be necessary. I don't know why TS complains.
-  readonly globalContext = new ContextNode('globalContext') as unknown as AnyContextNode<GlobalContextType>
-  readonly navigator: Navigator
+  private globalContext = new ContextNode('globalContext') as unknown as AnyContextNode<GlobalContextType>
 
-  addRoute = (properties: ScreenProperties) => {
-    const { method = 'get', path, screen } = properties
-    this.navigator.register(properties.screen, properties)
+  private addRoute = (screen: FC<any>, properties: RouteConfig) => {
+    const { method = 'get', path } = properties
     this.express[method](`${this.basePath}${path}`, (req, res) => {
       res.type('application/json')
       forEach(this.responseHeaders, (key, value) => res.setHeader(key, value))
@@ -45,9 +35,9 @@ export class BeagleApp<GlobalContextType = any> {
   }
 
   addRouteMap(routeMap: RouteMap) {
-    forEach(routeMap, (value, key) => this.addRoute({
-      path: key,
-      ...(typeof value === 'function' ? { screen: value } : value),
-    }))
+    forEach(routeMap, (value, key) => {
+      if (typeof value === 'function') this.addRoute(value, { path: key })
+      else this.addRoute(value.screen, { path: key })
+    })
   }
 }
