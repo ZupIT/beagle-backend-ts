6bd3d8fdc14a45d2448aaad16a77ba3f1883bc4c
Tiago Peres França <tiago.franca@zup.com.br>
2022-01-17 15:16:31 -0300

    ListView final interface

--- components/src/components/dynamic-list.tsx
+++ components/src/components/dynamic-list.tsx
@@ -1,28 +1,22 @@
 import {
   BeagleJSX,
   Actions,
-  FC,
   Expression,
-  Component,
   WithContext,
   AnyContextNode,
   ComponentProps,
+  ContextNode,
+  WithChildren,
+  DynamicExpression,
 } from '@zup-it/beagle-backend-core'
+import { Container } from '.'
 import { DefaultComponent } from '../default-component'
 import { WithStyle } from '../style/styled'
 import { WithAccessibility } from '../types'
 
-interface TemplateProps {
-  case?: Expression<boolean>,
-  children: JSX.Element,
+interface TemplateProps extends WithChildren {
+  case?: DynamicExpression<boolean>,
 }
-
-interface TemplateItem {
-  case?: Expression<boolean>,
-  view: JSX.Element,
-}
-
-type TemplateFC = (props: TemplateProps) => TemplateItem
 
 interface ListViewProps<T> extends WithContext, WithStyle, WithAccessibility {
   direction?: 'VERTICAL' | 'HORIZONTAL',
@@ -33,7 +27,7 @@ interface ListViewProps<T> extends WithContext, WithStyle, WithAccessibility {
   scrollEndThreshold?: number,
   iteratorName?: string,
   key?: string,
-  children: (item: AnyContextNode<T>) => TemplateItem,
+  children: (item: AnyContextNode<T>) => JSX.Element | JSX.Element[],
 }
 
 interface GridViewProps<T> extends ListViewProps<T> {
@@ -41,14 +35,40 @@ interface GridViewProps<T> extends ListViewProps<T> {
 }
 
 type ListFC = <T>(props: ComponentProps<ListViewProps<T>>) => JSX.Element
-type GridFC = <T>(props: ComponentProps<ListViewProps<T>>) => JSX.Element
+type GridFC = <T>(props: ComponentProps<GridViewProps<T>>) => JSX.Element
 
-export const Template: TemplateFC = props => ({ case: props.case, view: props.children })
-
-export const ListView: ListFC = ({ id, context, ...props }) => {
-  return <DefaultComponent name="listView" id={id} context={context} properties={props} />
+export const Template = (props: TemplateProps) => {
+  const children = Array.isArray(props.children) ? <Container>{props.children}</Container> : props.children
+  return <component name="template" namespace="pseudo" properties={{ case: props.case, view: children }} />
 }
 
-export const GridView: GridFC = ({ id, context, ...props }) => (
-  <DefaultComponent name="gridView" id={id} context={context} properties={props} />
+function getTemplates<T>(iteratorName = 'item', children: ListViewProps<T>['children']) {
+  let templateComponents = children(new ContextNode(iteratorName ?? 'item') as any)
+  if (!Array.isArray(templateComponents)) templateComponents = [templateComponents]
+  return templateComponents.map((templateComponent) => {
+    if (templateComponent.name !== 'template' || templateComponent.namespace !== 'pseudo') {
+      throw new Error(
+        `A ListView or GridView must only contain Template as children. Found ${templateComponent.namespace}:${templateComponent.name} instead.`,
+      )
+    }
+    return templateComponent.properties
+  })
+}
+
+export const ListView: ListFC = ({ id, context, children, ...props }) => (
+  <DefaultComponent
+    name="listView"
+    id={id}
+    context={context}
+    properties={{ ...props, templates: getTemplates(props.iteratorName, children) }}
+  />
+)
+
+export const GridView: GridFC = ({ id, context, children, ...props }) => (
+  <DefaultComponent
+    name="gridView"
+    id={id}
+    context={context}
+    properties={{ ...props, templates: getTemplates(props.iteratorName, children) }}
+  />
 )
--- sample/src/routes/test.tsx
+++ sample/src/routes/test.tsx
@@ -1,21 +1,7 @@
-import { Container, Text, Button, ListView } from '@zup-it/beagle-backend-components'
+import { Text } from '@zup-it/beagle-backend-components'
 import { Screen } from '@zup-it/beagle-backend-express'
 import { BeagleJSX } from '@zup-it/beagle-backend-core'
-import { alert } from '@zup-it/beagle-backend-core/actions/index'
-import { Template } from '../../../components/src/components/dynamic-list'
-
-interface Character {
-  name: string,
-  book: string,
-  age: number,
-  date: { year: number, month: number, day: number },
-  author: string
-}
-
-const characters: Character[] = []
 
 export const Test: Screen = () => (
-  <ListView dataSource={characters}>
-    {(item) => <Template><Text>{item.get('author')}</Text></Template>}
-  </ListView>
+  <Text>Hello World!</Text>
 )
