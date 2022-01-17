148b5c91653c0c9ebea79b0a2cc44605998f3c8b
Tiago Peres França <tiago.franca@zup.com.br>
2022-01-17 09:02:04 -0300

    initial work

--- components/src/components/dynamic-list.tsx
+++ components/src/components/dynamic-list.tsx
@@ -1,33 +1,54 @@
-import { BeagleJSX, Actions, FC, Expression, Component, WithContext } from '@zup-it/beagle-backend-core'
+import {
+  BeagleJSX,
+  Actions,
+  FC,
+  Expression,
+  Component,
+  WithContext,
+  AnyContextNode,
+  ComponentProps,
+} from '@zup-it/beagle-backend-core'
 import { DefaultComponent } from '../default-component'
 import { WithStyle } from '../style/styled'
 import { WithAccessibility } from '../types'
 
-interface TemplateManagerItem {
-  case: Expression<boolean>,
-  view: Component,
+interface TemplateProps {
+  case?: Expression<boolean>,
+  children: JSX.Element,
 }
 
-interface ListViewProps extends WithContext, WithStyle, WithAccessibility {
+interface TemplateItem {
+  case?: Expression<boolean>,
+  view: JSX.Element,
+}
+
+type TemplateFC = (props: TemplateProps) => TemplateItem
+
+interface ListViewProps<T> extends WithContext, WithStyle, WithAccessibility {
   direction?: 'VERTICAL' | 'HORIZONTAL',
   onInit?: Actions,
-  dataSource: Expression<any[]>,
-  templates: TemplateManagerItem[],
+  dataSource: Expression<T[]>,
   isScrollIndicatorVisible?: boolean,
   onScrollEnd?: Actions,
   scrollEndThreshold?: number,
   iteratorName?: string,
   key?: string,
+  children: (item: AnyContextNode<T>) => TemplateItem,
 }
 
-interface GridViewProps extends ListViewProps {
+interface GridViewProps<T> extends ListViewProps<T> {
   spanCount?: number,
 }
 
-export const ListView: FC<ListViewProps> = ({ id, context, ...props }) => (
-  <DefaultComponent name="listView" id={id} context={context} properties={props} />
-)
+type ListFC = <T>(props: ComponentProps<ListViewProps<T>>) => JSX.Element
+type GridFC = <T>(props: ComponentProps<ListViewProps<T>>) => JSX.Element
 
-export const GridView: FC<GridViewProps> = ({ id, context, ...props }) => (
+export const Template: TemplateFC = props => ({ case: props.case, view: props.children })
+
+export const ListView: ListFC = ({ id, context, ...props }) => {
+  return <DefaultComponent name="listView" id={id} context={context} properties={props} />
+}
+
+export const GridView: GridFC = ({ id, context, ...props }) => (
   <DefaultComponent name="gridView" id={id} context={context} properties={props} />
 )
--- sample/src/routes/test.tsx
+++ sample/src/routes/test.tsx
@@ -1,15 +1,21 @@
-import { Container, Text, Button } from '@zup-it/beagle-backend-components'
+import { Container, Text, Button, ListView } from '@zup-it/beagle-backend-components'
 import { Screen } from '@zup-it/beagle-backend-express'
 import { BeagleJSX } from '@zup-it/beagle-backend-core'
 import { alert } from '@zup-it/beagle-backend-core/actions/index'
+import { Template } from '../../../components/src/components/dynamic-list'
+
+interface Character {
+  name: string,
+  book: string,
+  age: number,
+  date: { year: number, month: number, day: number },
+  author: string
+}
+
+const characters: Character[] = []
 
 export const Test: Screen = () => (
-  <Container>
-    <Text>
-      Hello World!
-      Hi
-      Again
-    </Text>
-    <Button onPress={alert("hi")}>Click me</Button>
-  </Container>
+  <ListView dataSource={characters}>
+    {(item) => <Template><Text>{item.get('author')}</Text></Template>}
+  </ListView>
 )
