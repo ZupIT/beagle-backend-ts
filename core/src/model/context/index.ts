1802500f264912b0c64830163553ca2ec57b9c10
Tiago Peres França <tiago.franca@zup.com.br>
2022-01-18 16:51:31 -0300

    jsdocs for /jsx and /model

--- core/src/model/context/index.ts
+++
@@ -1,6 +0,0 @@
-import { AnyRootContext } from './types'
-import { RootContext } from './root-context'
-
-export function createContext<T>(id: string, value?: T): AnyRootContext<T> {
-  return new RootContext<T>(id, value) as any
-}
--- core/src/model/context/root-context.ts
+++ core/src/model/context/root-context.ts
@@ -1,5 +1,10 @@
 import { ContextNode } from './context-node'
+import { AnyRootContext } from './types'
 
+/**
+ * For better typing, you should use `createContext<T>()` instead of `new RootContext<T>()`. See
+ * {@link createContext}.
+ */
 export class RootContext<T> extends ContextNode<T> {
   constructor(id: string, value?: T) {
     super(id)
@@ -7,4 +12,57 @@ export class RootContext<T> extends ContextNode<T> {
   }
 
   readonly value?: T
+}
+
+/**
+ * A Context in a Beagle application is a data structure used to store values at runtime. It is equivalent to variables
+ * in a common programming language. It is important to remember that the context is only a reference, since the actual
+ * value will be calculated in the front-end only, when running the server-driven screen.
+ *
+ * This is used to create a new local context for the current tree of components. A Context must be declared by using
+ * the property "context" of a component. The scope of a context is only the component where it's declared and its
+ * children, i.e. it's not accessible by any component outside this scope.
+ *
+ * Example 1: a counter
+ * ```
+ * const counter = createContext('counter', 0)
+ *
+ * const MyScreen = () => (
+ *   <Container>
+ *     <Text>{`The button was pressed ${counter} times`}</Text>
+ *     <Button onPress={counter.set(sum(counter, 1))}>+1</Button>
+ *   </Container>
+ * )
+ * ```
+ *
+ * Example 2: loading user data
+ * ```
+ * interface User {
+ *   name: string,
+ *   address: { street: string, number: string },
+ * }
+ *
+ * const user = createContext<User>('user')
+ *
+ * const loadUser = sendRequest<User>({
+ *   url: 'https://myapi.com/user/1',
+ *   onSuccess: (response) => user.set(response.get('data')),
+ * })
+ *
+ * const MyScreen = () => (
+ *   <Container context={user} onInit={loadUser}>
+ *     <Text>{`Username: ${user.get('name')}`}</Text>
+ *     <Text>{`Address: ${user.get('address').get('street'), ${user.get('address').get('number')}`}</Text>
+ *   </Container>
+ * )
+ * ```
+ *
+ * Attention: it is very important to specify the type of the context if no initial value is provided, i.e. `T` must
+ * be set in `createContext<T>('id')`.
+ *
+ * @param id the id of the context, this is very important for debugging the application.
+ * @returns an instance of RootContext
+ */
+export function createContext<T>(id: string, value?: T): AnyRootContext<T> {
+  return new RootContext<T>(id, value) as any
 }
