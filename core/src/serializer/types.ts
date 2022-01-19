a4c0cdc81ee8df42fe67da99691b2ce144360771
Tiago Peres França <tiago.franca@zup.com.br>
2022-01-19 10:44:26 -0300

    adds jsdocs for core/serializer e core/

--- core/src/serializer/types.ts
+++ core/src/serializer/types.ts
@@ -1,16 +1,25 @@
 import { AnalyticsConfig } from '../model/action'
 
+/**
+ * A Context declaration as expected by the Beagle frontend libraries.
+ */
 export interface ContextDeclaration {
   id: string,
   value?: any,
 }
 
+/**
+ * A call to an Action as expected by the Beagle frontend libraries.
+ */
 export interface ActionCall {
   _beagleAction_: `${string}:${string}`,
   analytics?: AnalyticsConfig<any>,
   [key: string]: any,
 }
 
+/**
+ * A component as expected by the Beagle frontend libraries.
+ */
 export interface BeagleNode {
   _beagleComponent_: `${string}:${string}`,
   id?: string,
