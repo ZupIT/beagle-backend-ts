302a5b65e9ca4c78d9e9176d2c1566aecec0a7a0
james <jamesson.penha@zup.com.br>
2022-01-27 18:07:02 -0300

    Add pageView component

---
+++ components/src/components/page-view.tsx
@@ -0,0 +1,29 @@
+import { Actions, BeagleJSX, Expression, FC, WithChildren, WithContext } from '@zup-it/beagle-backend-core'
+import { DefaultComponent } from '../default-component'
+
+interface PageViewProps extends WithChildren, WithContext {
+  /**
+   * Actions to run when the selected page is changed.
+   */
+  onPageChange?: Actions,
+  /**
+   * The current page.
+   */
+  currentPage?: Expression<number>,
+  /**
+   * Whether or not to show page change arrows. Defaults to true.
+   */
+  showArrow?: boolean,
+}
+
+/**
+ * PageView presents pages that are displayed horizontally that can hold multiple components.
+ *
+ * @param props {@link PageIndicatorProps}.
+ * @returns JSX element, i.e an instance of Component.
+ */
+export const PageView: FC<PageViewProps> = ({ id, context, children, ...props }) => (
+  <DefaultComponent name="pageView" id={id} context={context} properties={props}>
+    {children}
+  </DefaultComponent>
+)
