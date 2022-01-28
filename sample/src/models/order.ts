3571aa66c1a99a7760116ec87d87fa83c11cd8ef
Tiago Peres França <tiago.franca@zup.com.br>
2022-01-28 17:49:05 -0300

    begginning to transform the sample in a real sample app

---
+++ sample/src/models/order.ts
@@ -0,0 +1,19 @@
+import { Product } from './product'
+
+interface Address {
+  zip: string,
+  street: string,
+  number: string,
+  city: string,
+  state: string,
+  complement: string,
+  neighborhood: string,
+}
+
+export interface Order {
+  id: number,
+  state: 'AWAITING_PAYMENT' | 'PAYMENT_ACCEPTED' | 'PREPARING' | 'SENT' | 'COMPLETED' | 'CANCELED',
+  products: Pick<Product, 'title' | 'price'>[],
+  total: number,
+  address: Address,
+}
