c28a37f85b4383775727a2b51af57be21d7e8112
Arthur Bleil <arthur.bleil@zup.com.br>
2022-02-07 11:03:15 -0300

    fix requested changes
    Signed-off-by: Arthur Bleil <arthur.bleil@zup.com.br>

--- components/src/components/utils/index.ts
+++ components/src/components/utils/index.ts
@@ -1,11 +1,10 @@
 import { isDynamicExpression } from '@zup-it/beagle-backend-core'
 import { InterpolatedText } from '../types'
 
-const stringable = (value: any) => !(typeof value === 'object')
+const stringable = (value: any) => typeof value !== 'object' || isDynamicExpression(value)
 const formatTextUnit = (text: any) => ((text && typeof text === 'object') ? JSON.stringify(text) : text ?? '')
 
-export const childrenToInterpolatedText = (children: InterpolatedText) => (
+export const childrenToInterpolatedText = (children: InterpolatedText) =>
   (Array.isArray(children) ? children : [children])
-    .map(child => stringable(child) ? child : (isDynamicExpression(child) ? child : formatTextUnit(child)))
+    .map(child => stringable(child) ? child : formatTextUnit(child))
     .join('')
-)
