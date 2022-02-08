456aa8c73110f486a8c89751344039d143afb3a6
Arthur Bleil <arthur.bleil@zup.com.br>
2022-02-08 11:03:29 -0300

    remove semicolons
    Signed-off-by: Arthur Bleil <arthur.bleil@zup.com.br>

--- express-client/src/utils/types.ts
+++ express-client/src/utils/types.ts
@@ -1,8 +1,8 @@
 export type IsRequired<T extends object, K extends keyof T> = T extends Record<K, T[K]> ? true : false
 
-export type IfAny<T, Y, N> = 0 extends (1 & T) ? Y : N;
+export type IfAny<T, Y, N> = 0 extends (1 & T) ? Y : N
 
-export type IsAny<T> = IfAny<T, true, never>;
+export type IsAny<T> = IfAny<T, true, never>
 
 export type HasRequiredProperty<T extends object> =
   { [K in keyof T]: IsRequired<T, K> } extends Record<string, false>
