73631b2cf169bd796599ced3c0e0e283a8ea55e1
Tiago Peres França <tiago.franca@zup.com.br>
2022-03-15 17:15:27 -0300

    modify boilerplates

--- cli/src/templates/boilerplate/src/index.ts
+++ cli/src/templates/boilerplate/src/index.ts
@@ -1,11 +1,15 @@
 import express from 'express'
 import cors from 'cors'
 import { BeagleApp } from '@zup-it/beagle-backend-express'
+import { setupHotReloading } from '@zup-it/beagle-backend-core'
 import configs from '../beagle-ts.json'
 import { routes as beagleRoutes } from './screens'
 
 const expressApp = express()
 expressApp.use(cors()).use(express.json())
-expressApp.listen(configs.port, () => console.log(`App listening at http://localhost:${configs.port}`))
+expressApp.listen(configs.port, () => {
+  console.log(`App listening at http://localhost:${configs.port}`)
+  setupHotReloading()
+})
 
 new BeagleApp(expressApp, beagleRoutes, { basePath: configs.basePath })
