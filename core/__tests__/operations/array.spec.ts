e286892b0f017b03ce1c390cf35d756fdeb385d1
Tiago Peres França <tiago.franca@zup.com.br>
2022-03-17 18:02:06 -0300

    tests for core/operations

--- core/__tests__/operations/array.spec.ts
+++ core/__tests__/operations/array.spec.ts
@@ -5,7 +5,7 @@ describe('Operations: array', () => {
   const arrayContext = createContextNode<string[]>('ctx')
 
   it('should create operation "contains"', () => {
-    const op = contains(arrayContext, 'test')
+    const op: Operation<boolean> = contains(arrayContext, 'test')
     expect(op).toBeInstanceOf(Operation)
     expect(op.name).toBe('contains')
     expect(op.args).toEqual([arrayContext, 'test'])
@@ -13,7 +13,7 @@ describe('Operations: array', () => {
 
   it('should create operation "insert"', () => {
     const element = createContextNode<string>('element')
-    const op = insert(arrayContext, element, 5)
+    const op: Operation<string[]> = insert(arrayContext, element, 5)
     expect(op).toBeInstanceOf(Operation)
     expect(op.name).toBe('insert')
     expect(op.args).toEqual([arrayContext, element, 5])
@@ -21,28 +21,28 @@ describe('Operations: array', () => {
 
   it('should create operation "insert" without an index', () => {
     const element = createContextNode<string>('element')
-    const op = insert(arrayContext, element)
+    const op: Operation<string[]> = insert(arrayContext, element)
     expect(op).toBeInstanceOf(Operation)
     expect(op.name).toBe('insert')
     expect(op.args).toEqual([arrayContext, element])
   })
 
   it('should create operation "remove"', () => {
-    const op = remove(arrayContext, 'test')
+    const op: Operation<string[]> = remove(arrayContext, 'test')
     expect(op).toBeInstanceOf(Operation)
     expect(op.name).toBe('remove')
     expect(op.args).toEqual([arrayContext, 'test'])
   })
 
   it('should create operation "removeIndex"', () => {
-    const op = removeIndex(arrayContext, 2)
+    const op: Operation<string[]> = removeIndex(arrayContext, 2)
     expect(op).toBeInstanceOf(Operation)
     expect(op.name).toBe('removeIndex')
     expect(op.args).toEqual([arrayContext, 2])
   })
 
   it('should create operation "removeIndex" without an index', () => {
-    const op = removeIndex(arrayContext)
+    const op: Operation<string[]> = removeIndex(arrayContext)
     expect(op).toBeInstanceOf(Operation)
     expect(op.name).toBe('removeIndex')
     expect(op.args).toEqual([arrayContext])
@@ -51,7 +51,7 @@ describe('Operations: array', () => {
   it('should create operation "union"', () => {
     const arrayContext2 = createContextNode<string[]>('ctx2')
     const arrayContext3 = createContextNode<string[]>('ctx3')
-    const op = union(arrayContext, arrayContext2, arrayContext3)
+    const op: Operation<string[]> = union(arrayContext, arrayContext2, arrayContext3)
     expect(op).toBeInstanceOf(Operation)
     expect(op.name).toBe('union')
     expect(op.args).toEqual([arrayContext, arrayContext2, arrayContext3])
--- core/__tests__/operations/logic.spec.ts
+++ core/__tests__/operations/logic.spec.ts
@@ -4,7 +4,7 @@ import { and, condition, not, or } from 'src/operations/logic'
 describe('Operations: logic', () => {
   it('should create operation "and"', () => {
     const boolCtx = createContextNode<boolean>('ctx')
-    const op = and(true, false, boolCtx)
+    const op: Operation<boolean> = and(true, false, boolCtx)
     expect(op).toBeInstanceOf(Operation)
     expect(op.name).toBe('and')
     expect(op.args).toEqual([true, false, boolCtx])
@@ -12,15 +12,15 @@ describe('Operations: logic', () => {
 
   it('should create operation "condition"', () => {
     const boolCtx = createContextNode<boolean>('ctx')
-    const op = condition(boolCtx, 'str', 5)
+    const op: Operation<string | number> = condition(boolCtx, 'str', 5)
     expect(op).toBeInstanceOf(Operation)
     expect(op.name).toBe('condition')
     expect(op.args).toEqual([boolCtx, 'str', 5])
   })
 
-  it('should create operation "condition"', () => {
+  it('should create operation "not"', () => {
     const boolCtx = createContextNode<boolean>('ctx')
-    const op = not(boolCtx)
+    const op: Operation<boolean> = not(boolCtx)
     expect(op).toBeInstanceOf(Operation)
     expect(op.name).toBe('not')
     expect(op.args).toEqual([boolCtx])
@@ -28,7 +28,7 @@ describe('Operations: logic', () => {
 
   it('should create operation "or"', () => {
     const boolCtx = createContextNode<boolean>('ctx')
-    const op = or(true, false, boolCtx)
+    const op: Operation<boolean> = or(true, false, boolCtx)
     expect(op).toBeInstanceOf(Operation)
     expect(op.name).toBe('or')
     expect(op.args).toEqual([true, false, boolCtx])
