f0cda399bba4b1c67a3074e115a2b4d8fa9dce58
Tiago Peres França <tiago.franca@zup.com.br>
2022-03-17 18:03:40 -0300

    rever test screen in sample

--- sample/src/beagle/screens/other/test.tsx
+++ sample/src/beagle/screens/other/test.tsx
@@ -1,9 +1,7 @@
-import { BeagleJSX, createContextNode } from '@zup-it/beagle-backend-core'
+import { BeagleJSX } from '@zup-it/beagle-backend-core'
 import { Text } from '@zup-it/beagle-backend-components'
 import { Screen } from '@zup-it/beagle-backend-express'
-import { insert } from '@zup-it/beagle-backend-core/operations'
 
-const arrayContext = createContextNode<string[]>('ctx')
 export const Test: Screen = () => (
-  <Text>Hello World! {insert(arrayContext, 'test')}</Text>
+  <Text>Hello World!</Text>
 )
