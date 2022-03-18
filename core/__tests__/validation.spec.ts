3c18ec9ebed66f40eb32b533afbe85aee2a63aa9
Tiago Peres França <tiago.franca@zup.com.br>
2022-03-18 17:29:11 -0300

    tests: global-context and utils

---
+++ core/__tests__/validation.spec.ts
@@ -0,0 +1,19 @@
+import { componentValidation } from 'src'
+
+describe('Component validation', () => {
+  it('should validate single component (success)', () => {
+
+  })
+
+  it('should validate single component (error)', () => {
+
+  })
+
+  it('should validate component tree (success)', () => {
+
+  })
+
+  it('should validate component tree (error)', () => {
+
+  })
+})
