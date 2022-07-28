c6160e2b5671652bea1075d797fbdfb37f63893e
Tiago Peres França <tiago.franca@zup.com.br>
2022-07-28 10:47:26 -0300

    a listview test

--- sample/src/beagle/operations.ts
+++ sample/src/beagle/operations.ts
@@ -1,4 +1,5 @@
 import { DynamicExpression, Expression, Operation } from '@zup-it/beagle-backend-core'
+import { ValidOperationAttribute } from '@zup-it/beagle-backend-core/model/operation/types'
 import { Product } from '../models/product'
 
 export const formatPrice = (price: Expression<number>, currency: string) => (
@@ -8,3 +9,8 @@ export const formatPrice = (price: Expression<number>, currency: string) => (
 export const sumProducts = (products: DynamicExpression<Product[]>) => (
   new Operation<number>('sumProducts', [products])
 )
+
+export const selectElementWithId = <T>(
+  list: DynamicExpression<T[]>,
+  id: DynamicExpression<any>,
+) => new Operation<T[]>('selectElementWithId', [list, id])
--- sample/src/beagle/screens/other/welcome.tsx
+++ sample/src/beagle/screens/other/welcome.tsx
@@ -1,22 +1,50 @@
-import { BeagleJSX } from '@zup-it/beagle-backend-core'
+import { BeagleJSX, createContext } from '@zup-it/beagle-backend-core'
 import { Screen } from '@zup-it/beagle-backend-express'
-import { pushView } from '@zup-it/beagle-backend-core/actions'
-import { Button, colors, Container, Image, Text } from '@zup-it/beagle-backend-components'
-import { Test } from './test'
+import { colors, Container, ListView, Template, Text, Touchable } from '@zup-it/beagle-backend-components'
+import { condition } from '@zup-it/beagle-backend-core/operations'
+import { selectElementWithId } from '../../operations'
 
-export const Welcome: Screen = () => (
-  <Container
-    style={{
-      flexDirection: 'COLUMN',
-      alignItems: 'CENTER',
-      justifyContent: 'CENTER',
-      height: 100,
-      backgroundColor: colors.white,
-    }}
-  >
-    <Image type="remote" url="https://i.ibb.co/rvRN9kv/logo.png" style={{ width: 242, height: 225 }} />
-    <Text style={{ marginTop: 40 }}>Welcome to the Beagle Playground!</Text>
-    <Text style={{ marginTop: 5 }}>Use the panel on the left to start coding!</Text>
-    <Button style={{ marginTop: 40 }} onPress={pushView('/docs.json')}>Access the fast guide</Button>
-  </Container>
-)
+const items = [
+  { id: '001', name: 'George', age: 32, selected: false },
+  { id: '003', name: 'Joana', age: 28, selected: false },
+  { id: '006', name: 'Philip', age: 45, selected: false },
+  { id: '025', name: 'Marianne', age: 62, selected: false },
+]
+
+export const Welcome: Screen = () => {
+  const people = createContext('people', items)
+  return (
+    <Container context={people}>
+      <ListView key="id" dataSource={people} style={{ borderColor: colors.black, padding: 10, borderWidth: 1 }}>
+        {(item) => (
+          <Template>
+            <Touchable onPress={people.set(selectElementWithId(people, item.get('id')))}>
+              <Container style={{
+                padding: 10,
+                borderColor: colors.black,
+                backgroundColor: condition(item.get('selected'), colors.aliceblue, colors.white)
+              }}>
+                <Text>Name: {item.get('name')}</Text>
+                <Text>Age: {item.get('age')}</Text>
+              </Container>
+            </Touchable>
+          </Template>
+        )}
+      </ListView>
+      <ListView key="id" dataSource={people} style={{ borderColor: colors.black, padding: 10, borderWidth: 1 }}>
+        {(item) => (
+          <Template>
+            <Container style={{
+              padding: 10,
+              borderColor: colors.black,
+              backgroundColor: condition(item.get('selected'), colors.aliceblue, colors.white)
+            }}>
+              <Text>Name: {item.get('name')}</Text>
+              <Text>Age: {item.get('age')}</Text>
+            </Container>
+          </Template>
+        )}
+      </ListView>
+    </Container>
+  )
+}
